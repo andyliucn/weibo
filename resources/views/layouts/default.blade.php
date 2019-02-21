ba2675a779a3d73cad842b14eaf10a8c614af634
andy <lc@yuanliutech.com>
2019-02-21 18:10:22 +0800

    完成用户注册功能

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,11 +5,12 @@
  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
  </head>
  <body>
- @include('layouts._header')
- <div class="container">
- <div class="offset-md-1 col-md-10">
- @yield('content')
- @include('layouts._footer')
+  @include('layouts._header')
+  <div class="container">
+  <div class="offset-md-1 col-md-10">
+   @include('shared._messages')
+   @yield('content')
+   @include('layouts._footer')
  </div>
  </div>
  </body>
